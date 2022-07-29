d22302351282f99a05cd0c764a8c4e71f7726fb0
rafaelupd <77645596+rafaelupd@users.noreply.github.com>
2022-07-29 17:57:36 -0300

    Create bypassdowsfileforwarder.js

---
+++ bypassdowsfileforwarder.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Autoclick dowsfile ad page (*.organvn.xyz)
+// @version      0.1
+// @description  Just an adaptation of bloggerpemula's Bypass All Shortlinks script.
+// @match        *://*.organvn.xyz/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const bp = query => document.querySelector(query);
+    const elementExists = query => bp(query) !== null;
+    function click(query) {bp(query).click();}
+    function clickIfElementExists(query, timeInSec = 0, funcName = 'setTimeout') {if (elementExists(query)) {window[funcName](function() {click(query);}, timeInSec * 1);}}
+
+    clickIfElementExists('#btn6.btn.btn-primary.btn-captcha')
+
+})();
