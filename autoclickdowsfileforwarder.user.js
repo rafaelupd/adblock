c33959ce1ef55aa8d73bd3aa51d5325bd6c20587
rafaelupd <77645596+rafaelupd@users.noreply.github.com>
2022-07-29 18:50:33 -0300

    Update and rename bypassdowsfileforwarder.user.js to autoclickdowsfileforwarder.user.js

--- autoclickdowsfileforwarder.user.js
+++ autoclickdowsfileforwarder.user.js
@@ -3,8 +3,8 @@
 // @version      0.1
 // @description  Just an adaptation of bloggerpemula's Bypass All Shortlinks script.
 // @match        *://*.organvn.xyz/*
-// @updateURL    https://raw.githubusercontent.com/rafaelupd/adblock/main/bypassdowsfileforwarder.user.js?token=GHSAT0AAAAAABWJC232JT7JAWQX6U44SN54YXEKVKQ
-// @downloadURL  https://raw.githubusercontent.com/rafaelupd/adblock/main/bypassdowsfileforwarder.user.js?token=GHSAT0AAAAAABWJC232JT7JAWQX6U44SN54YXEKVKQ
+// @updateURL    https://raw.githubusercontent.com/rafaelupd/adblock/main/bypassdowsfileforwarder.user.js
+// @downloadURL  https://raw.githubusercontent.com/rafaelupd/adblock/main/bypassdowsfileforwarder.user.js
 // @grant        none
 // ==/UserScript==
 
